cbbba9857938b34e334cc330657cd3e4f10c6c27
Mohammed Safwan <mdsafwan3102001@gmail.com>
2021-08-30 15:25:56 +0530

    Made some changes to add loading screen, error message

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,35 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
-
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products')
-            setProducts(data)
-        }
-        fetchProducts()
-    }, [])
+        dispatch(listProducts())
+    }, [dispatch])
 
+    const productList = useSelector(state => state.productList)
+
+    const { loading, error, products } = productList
+    
     return (
         <>
             <h1>New Products</h1>
-            <Row>
+            {loading ? (<h2>Loading....</h2>) : error ? (<Message variant = 'danger'>{error}</Message>) : 
+            (<Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3} className="align-items-stretch d-flex">
                         <Product product={product} />
                     </Col>
                 ))}
-            </Row>
+            </Row>)
+            
+            }
         </>
     )
 }
