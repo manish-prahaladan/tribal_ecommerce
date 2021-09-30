2a71618f1e53486733d8447587d89a3e79cf27f0
Mohammed Safwan S <mdsafwan3102001@gmail.com>
2021-09-30 12:57:37 +0530

    Implemented Profile update functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express, { Router } from 'express'
 const userRoutes = express.Router()
-import { authUser,getUserProfile,registerUser } from '../controllers/userController.js';
+import { authUser,getUserProfile,registerUser,updateUserProfile } from '../controllers/userController.js';
 import { protect } from "../middleware/authMiddleware.js"
 
 userRoutes.route('/').post(registerUser)
 userRoutes.post('/login',authUser)
-userRoutes.route('/profile').get(protect, getUserProfile)
+userRoutes.route('/profile').get(protect, getUserProfile).put(protect,updateUserProfile)
 
 export default userRoutes
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
-
+    const dispatch = useDispatch()
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+    const logoutHandler = ()=>{
+        dispatch(logout())
+    }
     return (
         <header>
             <Navbar bg="dark" expand="lg" variant="dark" collapseOnSelect>
@@ -15,9 +22,20 @@ const Header = () => {
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
                             <Nav.Link href="#home"><i className='fas fa-home' />Home</Nav.Link>
+                            {userInfo ? (
+                                <NavDropdown title = {userInfo.name} id = 'username'>
+                                    <LinkContainer to = "/profile">
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick = {logoutHandler}>
+                                        Logout
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                            ):
                             <LinkContainer to='/login'>
                                 <Nav.Link><i className='fas fa-user' />Login</Nav.Link>
-                            </LinkContainer>
+                            </LinkContainer>}
+                            
                             <LinkContainer to='/categories'>
                                 <Nav.Link><i className='fas fa-list-alt' />Categories</Nav.Link>
                             </LinkContainer>
