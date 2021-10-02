044ae181f095a63ca81ce0de162764faf5961bf5
Manish Prahaladan <80478435+manish-prahaladan@users.noreply.github.com>
2021-10-02 14:29:43 +0530

    Add files via upload

---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Welcome To Tribastore',
+  description: 'Shop here for the best tribal handicrafts and paintings',
+  keywords: 'paintings, handicrafts',
+}
+
+export default Meta
