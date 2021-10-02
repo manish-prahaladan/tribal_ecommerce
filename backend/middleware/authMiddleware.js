c56e1dc443c540682f8a1b26b0f96b090fd687e5
Manish Prahaladan <80478435+manish-prahaladan@users.noreply.github.com>
2021-10-02 14:21:31 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,27 +1,42 @@
-import  jwt  from "jsonwebtoken";
-import User from "../models/userModel.js";
-import asyncHandler from "express-async-handler";
-const protect = asyncHandler( async(req,res,next)=>{
-    let token
+import jwt from 'jsonwebtoken'
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
 
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        try{
-            token = req.headers.authorization.split(' ')[1];
-            const decoded = jwt.verify(token,process.env.JWT_SECRET)
-            req.user = await User.findById(decoded.id).select('-password')
-            next();
-        }
-        catch(err){
-            res.status(401)
-            console.log(err);
-            throw new Error("Unauthorized");
-        }
-        console.log("Token Found ")
+const protect = asyncHandler(async (req, res, next) => {
+  let token
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1]
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+      req.user = await User.findById(decoded.id).select('-password')
+
+      next()
+    } catch (error) {
+      console.error(error)
+      res.status(401)
+      throw new Error('Not authorized, token failed')
     }
-        if(!token){
-            res.status(401);
-            throw new Error('Unauthorised');
-        }
-    
+  }
+
+  if (!token) {
+    res.status(401)
+    throw new Error('Not authorized, no token')
+  }
 })
-export { protect }
+
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next()
+  } else {
+    res.status(401)
+    throw new Error('Not authorized as an admin')
+  }
+}
+
+export { protect, admin }
